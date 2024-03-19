2dce4ef705cd83b4ecf6fa0e196a9903b8ea6a93
Jannett <jarredondo0508@forsythtech.edu>
2024-03-18 20:59:12 -0400

    Initial Commit

---
+++ Purchasing/PurchasingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurchasingApp.swift
+//  Purchasing
+//
+//  Created by Jannett Arredondo on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PurchasingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
